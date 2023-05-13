e02d78044a3d4b8890a38453296c7c409346c868
bvigentas <brunovigentas@gmail.com>
2023-05-12 21:53:09 -0300

    remove old files

--- src/main/java/com/bvigentas/steam/handlers/ClientGCMsgHandler.java
+++
@@ -1,9 +0,0 @@
-package com.bvigentas.steam.handlers;
-
-import in.dragonbra.javasteam.base.IPacketGCMsg;
-
-public interface ClientGCMsgHandler {
-
-    void handleGCMsg(IPacketGCMsg packetGCMsg);
-
-}
